5ce29da7c33d556cc24c9f8b4ba5a8193d09d46b
saedteam <paghulor@gmail.com>
2024-02-29 04:55:34 -0800

    added html, css ad js codes

---
+++ script.js
@@ -0,0 +1,7 @@
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const slide = document.querySelector(".carousel-slide");
+
+let counter = 0;
+
+next.addEventListener('click', ()=>)
